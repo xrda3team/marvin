07d7b164e4cf02d3afe9eb8b9b5ad7ee65f0d870
Ajit Singh <info@xrda3.com>
2022-01-21 20:04:24 +0000

    Added init.cutf_cvm.rc to marvin

--- marvin/device.mk
+++ marvin/device.mk
@@ -17,5 +17,12 @@
 
 # This is based on device/google/cuttlefish/vsoc_x86/phone/aosp_cf.mk
 
+
+# If there are two PRODUCT_COPY_FILES rules for the same target file, the FIRST
+# ONE WINS. In the section below, we want to override some files that are copied
+# in emulator_vendor.mk so this MUST come before we inherit that file
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/init.cutf_cvm.rc:vendor/etc/init/hw/init.cutf_cvm.rc
+
 $(call inherit-product, device/google/cuttlefish/vsoc_x86/phone/aosp_cf.mk)
 
